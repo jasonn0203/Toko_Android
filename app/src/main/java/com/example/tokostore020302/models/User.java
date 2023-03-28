a6d1f5fedbd821727f95df30a884894a20a41f62
Jason Huynh <jasonn0203@gmail.com>
2023-03-28 12:54:09 +0700

    model User

---
+++ app/src/main/java/com/example/tokostore020302/models/User.java
@@ -0,0 +1,70 @@
+package com.example.tokostore020302.models;
+
+public class User {
+    private int id;
+    String firstname;
+    String lastname;
+    String email;
+
+    public User() {
+
+    }
+
+    public User(int id, String firstname, String lastname, String email, String address, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+    }
+
+    String address;
+    String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
