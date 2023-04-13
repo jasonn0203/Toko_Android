2bccff1afef0ad966aff48f2c5f50c9a29a90f3d
Jason Huynh <jasonn0203@gmail.com>
2023-04-13 16:25:23 +0700

    Update add/remove to cart !

--- app/src/main/java/com/example/tokostore020302/fragments/OrderFragment.java
+++ app/src/main/java/com/example/tokostore020302/fragments/OrderFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tokostore020302.Database.ProductDatabase;
 import com.example.tokostore020302.R;
@@ -20,35 +21,59 @@ import java.util.List;
 public class OrderFragment extends Fragment implements CartAdapter.OnItemClickListener {
 
     RecyclerView rvOrder;
+    TextView announce_order_text;
     CartAdapter cartAdapter;
+    ProductDatabase db;
+
+    List<Cart> cartList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_order, container, false);
 
+
+        announce_order_text = view.findViewById(R.id.order_text_announce);
         rvOrder = view.findViewById(R.id.rv_order);
         rvOrder.setLayoutManager(new LinearLayoutManager(getContext()));
-        List<Cart> cartList = getCartContents();
+
+
+        db = new ProductDatabase(getContext());
+        cartList = getCartContents();
         cartAdapter = new CartAdapter(cartList, getContext());
         cartAdapter.setListener(this);
         rvOrder.setAdapter(cartAdapter);
-        return view;
 
+        displayAnnounceText();
+
+
+        return view;
 
     }
 
+    //Hien thi danh sach sp trong gio hang
     public List<Cart> getCartContents() {
-        ProductDatabase db = new ProductDatabase(getContext());
+
         List<Cart> cartList = db.getCartContents();
         db.close();
+
+
         return cartList;
     }
 
 
     @Override
     public void onButtonDeleteClicked(Cart cartItem, int position) {
+        db.removeFromCart(cartItem.getProduct());
+        db.close();
+        if (position >= 0 && position < cartList.size()) {
+            cartList.remove(position);
+            cartAdapter.notifyItemRemoved(position);
+        }
 
+        displayAnnounceText();
+
+        cartAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -58,6 +83,14 @@ public class OrderFragment extends Fragment implements CartAdapter.OnItemClickLi
 
     @Override
     public void onQuantityDown(Cart cartItem, int position) {
+
+    }
+
+    void displayAnnounceText() {
+        if (cartList.size() > 0)
+            announce_order_text.setVisibility(View.GONE);
+        else
+            announce_order_text.setVisibility(View.VISIBLE);
 
     }
 }
